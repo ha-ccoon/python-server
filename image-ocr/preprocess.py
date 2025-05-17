939de32229de3cef4b309ef3e92fc70bf1ac496b
ha-ccoon <codeminhasohn@gmail.com>
2025-05-17 15:18:30 +0900

    feat: ocr image with openai

--- image-ocr/preprocess.py
+++ image-ocr/preprocess.py
@@ -1,5 +1,17 @@
 import cv2
+import os
+from PIL import Image
+import base64
+import io
+
 import matplotlib.pyplot as plt
+from openai import OpenAI
+from dotenv import load_dotenv
+
+
+load_dotenv() 
+
+client = OpenAI(api_key=os.getenv('OPEN_AI_API'))
 
 IMAGE_PATH = './image/'
 
@@ -28,10 +40,39 @@ def crop_image(image_path: str, coordinates: tuple):
     plt.title(f"Cropped Image - {cropped_image}")
     plt.axis('off')
     plt.show()
+    
+    pil_img = Image.fromarray(cv2.cvtColor(cropped_image, cv2.COLOR_BGR2RGB))
 
-    return cropped_image
-  
+
+    return pil_img
+
+
+def encode_image(image):
+    """PIL 이미지 객체를 base64로 인코딩"""
+    buffered = io.BytesIO()
+    image.save(buffered, format="PNG")
+    return base64.b64encode(buffered.getvalue()).decode('utf-8')
+
+
 
 if __name__ == "__main__":
     print("This is being run directly!")
-    crop_image(IMAGE_PATH + 'inbody.original.jpeg', (316, 326, 203, 49))
+    cropped = crop_image(IMAGE_PATH + 'inbody.original.jpeg', (316, 326, 203, 49))
+    
+    encode_image = encode_image(cropped)
+    
+    response = client.responses.create(
+      model="gpt-4.1-mini",
+      input=[{
+          "role": "user",
+          "content": [
+              {"type": "input_text", "text": "체중 값을 가져와줘"},
+              {
+                  "type": "input_image",
+                  "image_url": f"data:image/jpeg;base64,{encode_image}",
+              },
+          ],
+      }],
+    )
+    
+    print(response.output_text)
